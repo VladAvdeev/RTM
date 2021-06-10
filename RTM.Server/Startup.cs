5204b319ca7b618f64dfa57c99357ca5deb73854
VladAvdeev <vladislav.avdeev55@gmail.com>
2021-06-10 19:00:17 +0300

    add signalR

--- RTM.Server/Startup.cs
+++ RTM.Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RTM.Server.Hubs;
 using RTM.Server.Repositories;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -30,6 +31,7 @@ namespace RTM.Server
         {
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +51,7 @@ namespace RTM.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
 
             app.UseSwagger();
